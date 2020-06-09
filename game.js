513dce37f765568759a0a0974f1a062d2b828e0b
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-06-09 10:57:45 +0200

    Add gravity

--- game.js
+++ game.js
@@ -1,7 +1,22 @@
 initGame();
 
 function initGame() {
-
+    gravity();
     // Your game can start here, but define separate functions, don't write everything in here :)
 
+}
+
+
+function gravity() {
+    let speed = 5;
+    let y_pos = 0;
+    let pos = document.getElementById('bird');
+    let timer = setInterval(function () {
+        if (pos.style.top === '93%') {
+            clearInterval(timer);
+        } else {
+            speed++;
+            pos.style.top = speed + '%';
+        }
+    }, 50);
 }
