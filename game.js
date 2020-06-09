3c515eb08615929f6cdc6be732f2bd28b5e31969
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-06-09 14:24:22 +0200

    Change velocity

--- game.js
+++ game.js
@@ -1,24 +1,45 @@
 initGame();
 
 function initGame() {
-    gravity();
-    // Your game can start here, but define separate functions, don't write everything in here :)
-
+    document.getElementById('bird').style.top = '30%';
+    document.addEventListener('keypress', birdMove);
 }
 
-function gravity() {
+
+function gravity(stopTimer=false) {
     let pos = document.getElementById('bird');
-    let y_position = 30;
     let velocity = 0.01;
     let speed = 0.02;
     let timer = setInterval(function () {
+       let y_position = parseFloat(pos.style.top);
+        if (stopTimer) {
+            speed = -1;
+            velocity = 0;
+        }
+        velocity = velocity + 0.002;
+        speed = speed + velocity;
+        y_position = y_position + speed;
+        pos.style.top = y_position + '%';
         if (pos.style.top >= '91%') {
+            pos.style.top = '93%';
+            clearInterval(timer); //lose
+        }
+        if (pos.style.top <= '0%') {
+            pos.style.top = '0%';
             clearInterval(timer);
-        } else {
-            velocity = velocity + 0.002;
-            speed = speed + velocity;
-            y_position = y_position + speed;
-            pos.style.top = y_position + '%';
         }
     }, 40);
 }
+
+
+
+function birdMove(event) {
+    let stopTimer = false;
+    if (event.code === 'Space') {
+        event.preventDefault();
+        stopTimer = true;
+
+    }
+    gravity(stopTimer);
+}
+
