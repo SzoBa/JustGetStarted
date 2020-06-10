0929579ba9ab0c6b42d53c6a3f732e538512af44
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-06-10 11:55:00 +0200

    Add pipes

--- game.js
+++ game.js
@@ -1,35 +1,79 @@
 initGame();
 
 let gravityObject = {
-    gravity: 0.05,
-    gravitySpeed: 0
+    velocity: 0.05,
+    speed: 0
 }
 
 function initGame() {
     document.getElementById('bird').style.top = '30%';
-    // document.addEventListener('keypress', birdMove);
+    document.addEventListener('mousedown', birdUp);
+    document.addEventListener('mouseup', birdDown);
     gravity();
 }
 
 function gravity() {
-    let pos = document.getElementById('bird');
+    let actualBird = document.getElementById('bird');
     let timer = setInterval(function () {
-    let y_position = parseFloat(pos.style.top);
-        if (pos.style.top >= '91%') {
-            pos.style.top = '93%';
-            gravityObject.gravitySpeed = 0;
+        let y_position = parseFloat(actualBird.style.top);
+        if (actualBird.style.top >= '91%') {
+            actualBird.style.top = '93%';
+            gravityObject.speed = 0;
             clearInterval(timer);
         }
-        if (pos.style.top <= '1%') {
-            pos.style.top = '1%';
-            gravityObject.gravitySpeed = 0;
+        if (actualBird.style.top <= '1%') {
+            actualBird.style.top = '1%';
+            gravityObject.speed = 0;
         }
-        gravityObject.gravitySpeed = gravityObject.gravitySpeed + gravityObject.gravity;
-        y_position = y_position + gravityObject.gravitySpeed;
-        pos.style.top = y_position + '%';
+        gravityObject.speed = gravityObject.speed + gravityObject.velocity;
+        y_position = y_position + gravityObject.speed;
+        actualBird.style.top = y_position + '%';
+
+        pipeCheck();
+
+
+        let bird = document.getElementById('bird');
+        if (gravityObject.speed > 1 && !bird.classList.contains('birdUp')) {
+        bird.classList.add('birdDown');
+        }
     }, 40);
 }
 
 function accelerate(n) {
-    gravityObject.gravity = n;
+    gravityObject.velocity = n;
+}
+
+function birdUp() {
+    accelerate(-0.2);
+    let bird = document.getElementById('bird');
+    bird.classList.remove('birdDown');
+    bird.classList.add('birdUp');
+}
+
+
+function birdDown() {
+    accelerate(0.05);
+    let bird = document.getElementById('bird');
+    bird.classList.remove('birdUp');
+}
+
+
+function pipeCheck() {
+    let gameArea = document.getElementById('container');
+    let northPipes = document.getElementsByClassName('pipeNorth');
+    let southPipes = document.getElementsByClassName('pipeSouth')
+    for (let northPipe of northPipes) {
+        if (northPipe.getBoundingClientRect().x < gameArea.getBoundingClientRect().x) {
+            northPipe.classList.add('hidden');
+        } else if (northPipe.classList.contains('hidden')) {
+            northPipe.classList.remove('hidden');
+        }
+    }
+    for (let southPipe of southPipes) {
+        if (southPipe.getBoundingClientRect().x < gameArea.getBoundingClientRect().x) {
+            southPipe.classList.add('hidden');
+        } else if (southPipe.classList.contains('hidden')) {
+            southPipe.classList.remove('hidden');
+        }
+    }
 }
