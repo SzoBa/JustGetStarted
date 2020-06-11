0a0d1903fd5f8420022aca37e02b4fb89ffc88db
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-06-11 09:44:02 +0200

    Add random pipe function

--- game.js
+++ game.js
@@ -3,7 +3,8 @@ initGame();
 let generalObject = {
     velocity: 0.05,
     speed: 0,
-    points: 0
+    points: 0,
+    collision: false
 }
 
 
@@ -19,25 +20,30 @@ function gravity() {
     let actualBird = document.getElementById('bird');
     let timer = setInterval(function () {
         let y_position = parseFloat(actualBird.style.top);
-        if (actualBird.style.top >= '91%') {
-            actualBird.style.top = '93%';
-            generalObject.speed = 0;
-            lose();
-        }
-        if (actualBird.style.top <= '1%') {
-            actualBird.style.top = '1%';
-            generalObject.speed = 0;
-        }
-        generalObject.speed = generalObject.speed + generalObject.velocity;
-        y_position = y_position + generalObject.speed;
-        actualBird.style.top = y_position + '%';
+        if (generalObject.collision) {
+            clearInterval(timer);
+        } else {
+            if (actualBird.style.top >= '91%') {
+                actualBird.style.top = '93%';
+                generalObject.speed = 0;
+                generalObject.collision = true;
+                lose();
+            }
+            if (actualBird.style.top <= '1%') {
+                actualBird.style.top = '1%';
+                generalObject.speed = 0;
+            }
+            generalObject.speed = generalObject.speed + generalObject.velocity;
+            y_position = y_position + generalObject.speed;
+            actualBird.style.top = y_position + '%';
 
-        pipeCheck();
-        collisionDetection();
+            pipeCheck();
+            collisionDetection();
 
-        let bird = document.getElementById('bird');
-        if (generalObject.speed > 1 && !bird.classList.contains('birdUp')) {
-        bird.classList.add('birdDown');
+            let bird = document.getElementById('bird');
+            if (generalObject.speed > 1 && !bird.classList.contains('birdUp')) {
+                bird.classList.add('birdDown');
+            }
         }
     }, 40);
 }
@@ -67,46 +73,34 @@ function pipeCheck() {
     let gameArea = document.getElementById('container');
     let pipes = document.getElementsByClassName('pipe');
     for (let pipe of pipes) {
-        if (pipe.getBoundingClientRect().x < gameArea.getBoundingClientRect().x) {
-            pipe.classList.add('hidden');
+        if (pipe.getBoundingClientRect().x <= (gameArea.getBoundingClientRect().x + 10)) {
+            pipe.remove();
             increaseScore();
-        } else if (pipe.classList.contains('hidden')) {
-            pipe.classList.remove('hidden');
-
         }
     }
 }
 
 function pipeHandler() {
+    let pipePool = [['pipeNorth1', 'pipeSouth1'], ['pipeNorth2', 'pipeSouth2'], ['pipeNorth3', 'pipeSouth3']];
     let gameArea = document.getElementById('container');
-    let pipeNorth1 = document.createElement('div');
-    pipeNorth1.classList.add('pipe');
-    pipeNorth1.setAttribute('id', 'pipeNorth1');
-    let pipeSouth1 = document.createElement('div');
-    pipeSouth1.classList.add('pipe');
-    pipeSouth1.setAttribute('id', 'pipeSouth1');
-    gameArea.appendChild(pipeNorth1);
-    gameArea.appendChild(pipeSouth1);
-    setTimeout(function () {
-        let pipeNorth2 = document.createElement('div');
-        pipeNorth2.classList.add('pipe');
-        pipeNorth2.setAttribute('id', 'pipeNorth2');
-        let pipeSouth2 = document.createElement('div');
-        pipeSouth2.classList.add('pipe');
-        pipeSouth2.setAttribute('id', 'pipeSouth2');
-        gameArea.appendChild(pipeNorth2);
-        gameArea.appendChild(pipeSouth2);
-    }, 3000);
-    setTimeout(function () {
-        let pipeNorth3 = document.createElement('div');
-        pipeNorth3.classList.add('pipe');
-        pipeNorth3.setAttribute('id', 'pipeNorth3');
-        let pipeSouth3 = document.createElement('div');
-        pipeSouth3.classList.add('pipe');
-        pipeSouth3.setAttribute('id', 'pipeSouth3');
-        gameArea.appendChild(pipeNorth3);
-        gameArea.appendChild(pipeSouth3);
-    }, 6000);
+    let pipeTimer = setInterval(function () {
+        if (generalObject.collision) {
+            clearInterval(pipeTimer);
+        } else {
+            let randomChoice = Math.floor(Math.random() * pipePool.length);
+            let actualPipeNorth = pipePool[randomChoice][0];
+            let actualPipeSouth = pipePool[randomChoice][1];
+            let pipeNorth1 = document.createElement('div');
+            pipeNorth1.classList.add('pipe');
+            pipeNorth1.setAttribute('id', actualPipeNorth);
+            let pipeSouth1 = document.createElement('div');
+            pipeSouth1.classList.add('pipe');
+            pipeSouth1.setAttribute('id', actualPipeSouth);
+            gameArea.appendChild(pipeNorth1);
+            gameArea.appendChild(pipeSouth1);
+        }
+    }, 2000);
+    automaticStop();
 }
 
 function collisionDetection() {
@@ -117,21 +111,29 @@ function collisionDetection() {
         if (pipe.id.includes('North')) {
             if (birdPositions.right >= pipePosition.x && (birdPositions.top < pipePosition.bottom) && birdPositions.x < pipePosition.right) {
                 lose();
+                generalObject.collision = true;
             }
         } else if (pipe.id.includes('South')){
             if (birdPositions.right >= pipePosition.x && (birdPositions.bottom > pipePosition.top) && birdPositions.x < pipePosition.right) {
                 lose();
+                generalObject.collision = true;
             }
         }
     }
 }
 
 function lose() {
-    let loseScreen = document.createElement('div');
-    loseScreen.innerHTML = 'YOU LOST';
-    loseScreen.classList.add('loseScreen');
-    document.getElementById('container').remove();
-    document.getElementsByTagName('main')[0].appendChild(loseScreen);
+    // let loseScreen = document.createElement('div');
+    // loseScreen.innerHTML = 'YOU LOST';
+    // loseScreen.classList.add('loseScreen');
+    // document.getElementById('container').remove();
+    // document.getElementsByTagName('main')[0].appendChild(loseScreen);
+    document.removeEventListener('mousedown', birdUp);
+    document.removeEventListener('mouseup', birdDown);
+    let gameOver = document.createElement('div');
+    gameOver.innerHTML = 'Game Over';
+    gameOver.setAttribute('id', 'game-over');
+    document.getElementById('container').appendChild(gameOver);
 }
 
 
@@ -140,4 +142,19 @@ function increaseScore() {
     score++;
     generalObject.points = score;
     document.getElementById('score').innerHTML = score;
+}
+
+function automaticStop() {
+    let autoStop = setInterval(function () {
+        if (generalObject.collision) {
+            let pipes = document.getElementsByClassName('pipe');
+            for (let pipe of pipes) {
+                pipe.style.animationPlayState = "paused";
+                pipe.classList.remove('pipe');
+            }
+            if (pipes.length === 0) {
+                clearInterval(autoStop);
+            }
+        }
+    },20);
 }
